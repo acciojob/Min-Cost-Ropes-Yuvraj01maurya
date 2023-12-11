01837ec8ef780e34e8e6d6b75d7b9b39c21ce303
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-11 17:40:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,30 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  if (arr.length === 0) return 0;
+
+  // Sort the array in ascending order initially
+  arr.sort((a, b) => a - b);
+
+  let cost = 0;
+
+  while (arr.length > 1) {
+    // Find the two shortest ropes
+    const first = arr.shift();
+    const second = arr.shift();
+
+    // Merge the two ropes and add the cost
+    const sum = first + second;
+    cost += sum;
+
+    // Add the merged rope back to the array
+    arr.push(sum);
+
+    // Resort the array after adding the new rope
+    arr.sort((a, b) => a - b);
+  }
+
+  return cost;
 }
 
 module.exports=mincost;
